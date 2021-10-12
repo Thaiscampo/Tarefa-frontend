0f70d47d41d80b711440024a83bbb1b6e4112b0e
Thaiscampo <thais.campo123@gmail.com>
2021-10-12 18:21:12 -0300

    tarefa atualizada

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import api from '../../services/api';
 import moment from 'moment';
+import './index.css';
  
 interface ITask{
     id: number;
@@ -15,6 +17,7 @@ interface ITask{
 const Tasks: React.FC = () => {
  
     const [tasks, setTasks] = useState<ITask[]>([])
+    const history = useHistory()
  
     useEffect(() => {
         loadTasks()
@@ -30,17 +33,29 @@ const Tasks: React.FC = () => {
         return moment(date).format('DD/MM/YYYY')
     }
  
+    function newTask(){
+        history.push('/tarefas_cadastro')
+    }
+ 
+    function editTask(id: number){
+        history.push(`/tarefas_cadastro/${id}`)
+    }
+ 
     return (
         
         <div className="container">
             <br />
-            <h1>Página de Tarefas</h1>
+            <div className="task-header">
+                <h1>Tarefas</h1>
+                <Button variant="dark" size="sm" onClick={newTask}>Nova Tarefa</Button>
+            </div>
             <br />
             <Table striped bordered hover className="text-center">
                 <thead>
                     <tr>
                     <th>ID</th>
                     <th>Título</th>
+                    <th>Descrição</th>
                     <th>Data de Atualização</th>
                     <th>Status</th>
                     <th>Ações</th>
@@ -52,10 +67,11 @@ const Tasks: React.FC = () => {
                             <tr key={task.id}>
                                 <td>{task.id}</td>
                                 <td>{task.title}</td>
+                                <td>{task.description}</td>
                                 <td>{formatDate(task.updated_at)}</td>
                                 <td>{task.finished ? "Finalizado" : "Pendente"}</td>
                                 <td>
-                                    <Button size="sm" variant="primary">Editar</Button>{' '}
+                                    <Button size="sm" variant="primary" onClick={() => editTask(task.id)}>Editar</Button>{' '}
                                     <Button size="sm" variant="success">Finalizar</Button>{' '}
                                     <Button size="sm" variant="warning">Visualizar</Button>{' '}
                                     <Button size="sm" variant="danger">Remover</Button>{' '}
